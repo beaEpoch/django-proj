631e093f2b873de872e6a46e843a69dcfe2bb64f
apple <apple@appledeMacBook-Air.local>
2019-05-21 19:40:16 +0800

    fix js bug

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'b0&g0@sgkwd3ecu4vp*ly!=9a%u^4kfl$b&f43@-sbhplbo8$r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
